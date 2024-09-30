65cde7fd7524325b05d074f8d7323725fb285cd2
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-09-30 12:59:06 -0500

    Read database

--- src/main.py
+++ src/main.py
@@ -1,5 +1,36 @@
-def main() -> None:
-    pass
+from pathlib import Path
+
+import click
+from modin import pandas as pd
+from modin.pandas import DataFrame
+from sqlalchemy import Engine, create_engine
+
+
+def readDB(dbEngine: Engine) -> DataFrame:
+    return pd.read_sql_table(table_name="documents", con=dbEngine)
+
+
+@click.command()
+@click.option(
+    "-i",
+    "--input",
+    "inputPath",
+    help="Path to arXiv SQLite3 database",
+    type=click.Path(
+        exists=True,
+        file_okay=True,
+        readable=True,
+        resolve_path=True,
+        path_type=Path,
+    ),
+    required=True,
+)
+def main(inputPath: Path) -> None:
+    dbEngine: Engine = create_engine(url=f"sqlite:///{inputPath}")
+
+    df: DataFrame = readDB(dbEngine=dbEngine)
+
+    print(df)
 
 
 if __name__ == "__main__":
