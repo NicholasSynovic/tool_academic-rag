a5813b9bd07ed09db5114597668a4f6109290a61
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-09-30 13:54:45 -0500

    Add code to write embeddings to pickle file

--- src/main.py
+++ src/main.py
@@ -1,9 +1,31 @@
+import pickle  # nosec
 from pathlib import Path
+from typing import Any
 
 import click
 from modin import pandas as pd
-from modin.pandas import DataFrame
+from modin.pandas import DataFrame, Series
+from numpy import ndarray
+from sentence_transformers import SentenceTransformer
 from sqlalchemy import Engine, create_engine
+
+
+def embed(modelID: str, content: Series) -> ndarray:
+    model: SentenceTransformer = SentenceTransformer(
+        model_name_or_path=modelID,
+    )
+
+    pool: dict[str, Any] = model.start_multi_process_pool()
+
+    data: ndarray = model.encode_multi_process(
+        sentences=content.to_list(),
+        show_progress_bar=True,
+        pool=pool,
+    )
+
+    model.stop_multi_process_pool(pool=pool)
+
+    return data
 
 
 def readDB(dbEngine: Engine) -> DataFrame:
@@ -25,12 +47,28 @@ def readDB(dbEngine: Engine) -> DataFrame:
     ),
     required=True,
 )
-def main(inputPath: Path) -> None:
+@click.option(
+    "-m",
+    "--model",
+    "modelName",
+    help="Name of the embedding model",
+    type=str,
+    required=False,
+    default="all-mpnet-base-v2",
+    show_default=True,
+)
+def main(inputPath: Path, modelName: str) -> None:
     dbEngine: Engine = create_engine(url=f"sqlite:///{inputPath}")
 
     df: DataFrame = readDB(dbEngine=dbEngine)
 
-    print(df)
+    abstracts: Series = df["abstract"]
+
+    embeddings: ndarray = embed(modelID=modelName, content=abstracts)
+
+    with open("embeddings.ndarray.pickle", "wb") as pf:
+        pickle.dump(obj=embeddings, file=pf)
+        pf.close()
 
 
 if __name__ == "__main__":
